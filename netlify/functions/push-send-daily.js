f1a515c15bd360fcf5a1a0ed66a3212865998faf
maktabsains125 <maktabsains125@gmail.com>
2026-04-19 06:10:36 +0800

    Update push-send-daily.js

--- netlify/functions/push-send-daily.js
+++ netlify/functions/push-send-daily.js
@@ -1,6 +1,6 @@
 const webpush = require("web-push");
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwSJCVKS5TOjXd_3C--O3xmBDxuopeX4TCxksx8mnc_LtDvSq1K4krIBubw85UptEzf/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwb3p5U5vz1IC5ua6UVI7EbVslTIhhVmr0-quaNGJy3scyyeP-oGpxajqf1Yd5Rz_sI/exec";
 
 if (!process.env.VAPID_PUBLIC_KEY || !process.env.VAPID_PRIVATE_KEY) {
   throw new Error("Missing VAPID_PUBLIC_KEY or VAPID_PRIVATE_KEY");
